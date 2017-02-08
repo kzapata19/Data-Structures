fae0743d4186789d46f2023089d5992f6d3bf2e2
Karen Zapata <kzapata0919@gmail.com>
2017-02-08 00:46:51 -0800

    Remove comments

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,34 +1,3 @@
-// var BinarySearchTree = function(value) {
-//   this.value = value;
-//   this.left = null;
-//   this.right = null;
-// };
-
-// BinarySearchTree.prototype.insert = function(value){
-//   if(value <= this.value){
-//     if(!this.left) this.left = new BinarySearchTree(value)
-//     else this.left.insert(value);
-//   } else {
-//     if(!this.right) this.right = new BinarySearchTree(value)
-//     else this.right.insert(value)
-//   }
-// };
-
-// BinarySearchTree.prototype.contains = function(value){
-//   if(value === this.value) return true
-//   else if(value < this.value){
-//     if(!this.left) return false
-//     else this.left.contains(value)
-//   } else {
-//     if(!this.right) return false
-//     else this.right.contains(value)
-//   }
-// };
-
-// BinarySearchTree.prototype.depthFirstLog = function(callback){
-
-// };
-
 var BinarySearchTree = function(value){
   var tree = Object.create(BinarySearchTree.prototype);
   tree.value = value;
