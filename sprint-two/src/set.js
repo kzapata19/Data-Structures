7c2f96203c58deb6b6dea19bb8a71e446e7ea326
Karen Zapata <kzapata0919@gmail.com>
2017-02-20 22:58:43 -0800

    Add implementation of set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if(!this.contains(item)){
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  return item in this._storage;
 };
 
 setPrototype.remove = function(item) {
+  if(this.contains(item)){
+    delete this._storage[item];
+  }
 };
 
 /*
