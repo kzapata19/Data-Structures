6ce7887f6873b24502a4ec14a78bae81c9e8a152
Karen Zapata <kzapata0919@gmail.com>
2017-03-30 14:55:17 -0700

    Complete addToHead method for doubly linked list in pseudoclassical pattern

--- sprint-two/src/pseudoclassical/doubly-linkedList.js
+++ sprint-two/src/pseudoclassical/doubly-linkedList.js
@@ -12,15 +12,19 @@ var Node = function(value){
 
 DoublyLinkedList.prototype.addToHead = function(value){
   var node = new Node(value);
-  this.length++;
   if(this.isEmpty()){
     this.head = node;
     this.tail = node;
   } else {
-    var formerHead = this.head;
-
+      var formerHead = this.head;
+      node.next = formerHead;
+      formerHead.previous = node;
+      this.head = node;
+      if(this.length === 1){
+        this.tail = formerHead;
+      }
   }
-
+  this.length++;
 };
 
 DoublyLinkedList.prototype.addToTail = function(value){
@@ -32,7 +36,19 @@ DoublyLinkedList.prototype.insert = function(position, value){
 };
 
 DoublyLinkedList.prototype.removeHead = function(){
-
+  if(!this.isEmpty()){
+    var toDelete = this.head;
+    this.length--;
+    if(!this.head.next){
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = toDelete.next;
+      toDelete.previous = null;
+    }
+    return toDelete.value;
+  }
+  return null;
 };
 
 DoublyLinkedList.prototype.removeTail = function(){
@@ -79,7 +95,7 @@ DoublyLinkedList.prototype.retrieveTail = function(){
 };
 
 DoublyLinkedList.prototype.isEmpty = function(){
-  return this.length === 0;
+  return this.size() === 0;
 };
 
 DoublyLinkedList.prototype.size = function(){
@@ -90,3 +106,10 @@ DoublyLinkedList.prototype.toString = function(){
 
 };
 
+var myDD = new DoublyLinkedList();
+console.log(myDD.addToHead("one"));
+console.log("after one:", myDD);
+console.log(myDD.addToHead("two"));
+console.log("after two: ", myDD)
+console.log(myDD.addToHead("three"));
+console.log("after three: ", myDD)
