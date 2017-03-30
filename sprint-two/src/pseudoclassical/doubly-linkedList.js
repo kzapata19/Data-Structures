c32158c30523d423b3534beca81f558ec4505307
Karen Zapata <kzapata0919@gmail.com>
2017-03-29 21:49:20 -0700

    Add doubly linked list methods in pseudoclassical pattern

---
+++ sprint-two/src/pseudoclassical/doubly-linkedList.js
@@ -0,0 +1,67 @@
+var DoublyLinkedList = function(){
+  this.head = null;
+  this.tail = null;
+  this.length = 0;
+};
+
+var Node = function(value){
+  this.value = value;
+  this.previous = null;
+  this.next = null;
+};
+
+DoublyLinkedList.prototype.addToHead = function(value){
+
+};
+
+DoublyLinkedList.prototype.addToTail = function(value){
+
+};
+
+DoublyLinkedList.prototype.insert = function(position, value){
+
+};
+
+DoublyLinkedList.prototype.removeHead = function(){
+
+};
+
+DoublyLinkedList.prototype.removeTail = function(){
+
+};
+
+DoublyLinkedList.prototype.remove = function(value){
+
+};
+
+DoublyLinkedList.prototype.removeAt = function(position){
+
+};
+
+DoublyLinkedList.prototype.indexOf = function(value){
+
+};
+
+DoublyLinkedList.prototype.contains = function(target){
+
+};
+
+DoublyLinkedList.prototype.retrieveHead = function(){
+
+};
+
+DoublyLinkedList.prototype.retrieveTail = function(){
+
+};
+
+DoublyLinkedList.prototype.isEmpty = function(){
+
+};
+
+DoublyLinkedList.prototype.size = function(){
+
+};
+
+DoublyLinkedList.prototype.toString = function(){
+
+};
